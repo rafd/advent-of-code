228a464cc8be6edd26538a85be03d47d4d6ac150
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-12-27 18:50:46 -0500

    Tune 2021 Day 17 by improving bounds
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

--- src/aoc/year2021/day17.clj
+++ src/aoc/year2021/day17.clj
@@ -10,25 +10,23 @@
   target-x-max)
 
 ;; TODO can improve
-(defn steps-lower-bound [_ _]
-  1)
+(defn steps-lower-bound [[_ target-x-max _ _] velocity-x]
+ (int (quot target-x-max velocity-x)))
 
 ;; TODO can improve
-(defn steps-upper-bound [[_ target-x-max _ _ :as target-box] velocity-x]
-  (inc (velocity-x-upper-bound target-box))
-  #_(Math/ceil (/ (+ -1 (Math/sqrt (+ 1 (* 8 target-x-max))))
-                  2)))
+(defn steps-upper-bound [[target-x-min target-x-max _ _ :as target-box] velocity-x]
+  (inc (velocity-x-upper-bound target-box)))
+
+(defn velocity-y-lower-bound [[target-x-min target-x-max target-y-min _] velocity-x]
+  #_target-y-min
+  #_(quot target-y-min
+          (inc (quot target-x-min (inc velocity-x))))
+  (+ 1 (quot (- target-y-min 1)
+             (inc (quot target-x-min (inc velocity-x))))))
 
 ;; TODO can improve
-(defn velocity-y-lower-bound [[_ _ target-y-min _] _]
-  target-y-min)
-
-;; TODO can improve
-(defn velocity-y-upper-bound [[_ _ _ target-y-max] steps]
-  (* 3 ;; YOLO
-    (Math/floor (+ (/ target-y-max steps)
-                   (/ (- steps 1)
-                      2)))))
+(defn velocity-y-upper-bound [[_ _ target-y-min _] velocity-x]
+  (- target-y-min))
 
 (defn hits-target? [[target-x-min target-x-max
                      target-y-min target-y-max]
@@ -60,10 +58,8 @@
 (defn velocity-bounded-candidates [target-box]
   (->> (for [velocity-x (range (velocity-x-lower-bound target-box)
                                (inc (velocity-x-upper-bound target-box)))
-             steps (range (steps-lower-bound target-box velocity-x)
-                          (inc (steps-upper-bound target-box velocity-x)))
-             velocity-y (range (velocity-y-lower-bound target-box steps)
-                               (inc (velocity-y-upper-bound target-box steps)))]
+             velocity-y (range (velocity-y-lower-bound target-box velocity-x)
+                               (inc (velocity-y-upper-bound target-box velocity-x)))]
          [velocity-x velocity-y])
        distinct))
 
@@ -108,11 +104,14 @@
    (dorun
     (for [y (range min-y (inc max-y))]
      (swap! s (fn [o] (apply str o "\n" (pad y) (for [x (range min-x (inc max-x))]
-                                                  (pad (or (hits-target? target-box [x y])
-                                                           (if (contains? p [x y])
-                                                            "."
-                                                            " ")))))))))
-   (spit "plot.txt" @s))
+                                                  (pad (let [hits? (hits-target? target-box [x y])
+                                                             considered? (contains? p [x y])]
+                                                         (cond
+                                                          (and hits? considered?) hits?
+                                                          considered? "."
+                                                          hits? (str "-" hits?)
+                                                          :else " ")))))))))
+   (spit "plot3.txt" @s))
 
 ;; part 1
 #_(optimize-velocity (get-target-box))
