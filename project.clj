78eebc9c94fec033e5058d6613c150faebb0fcb8
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-12-26 21:46:08 -0500

    Add ultra lib
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject aoc "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/data.priority-map "1.1.0"]])
+                 [org.clojure/data.priority-map "1.1.0"]]
+
+  ;; LEIN_USE_BOOTCLASSPATH=no lein repl
+  :plugins [[venantius/ultra "0.6.0"]])
