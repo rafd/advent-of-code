91e0a29c28df63c407487f355fb73b10169ef908
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-01-02 14:48:36 -0500

    Solve 2017 Day 1
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

---
+++ src/aoc/year2017/day1.clj
@@ -0,0 +1,23 @@
+(ns aoc.year2017.day1
+ (:require
+   [aoc.helpers :refer [parse-input]]))
+
+;; part 1
+
+#_(->> (parse-input 2017 1 "")
+       (map #(Integer. %))
+       ((fn [x] (partition 2 1 x x)))
+       (filter (fn [[x y]] (= x y)))
+       (map first)
+       (reduce +))
+
+;; part 2
+
+#_(let [numbers (->> (parse-input 2017 1 "")
+                     (map #(Integer. %)))]
+   (->> (split-at (/ (count numbers) 2) numbers)
+        (apply map vector)
+        (filter (fn [[x y]] (= x y)))
+        (map first)
+        (reduce +)
+        (* 2)))
