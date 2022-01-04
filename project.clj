38076f347cdc2e4bf4136a2953c7cb5fe5a23c89
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-01-04 13:53:49 -0500

    Solve 2017 Day 2
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject aoc "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/data.priority-map "1.1.0"]]
+                 [org.clojure/data.priority-map "1.1.0"]
+                 [org.clojure/math.combinatorics "0.1.6"]]
 
   ;; LEIN_USE_BOOTCLASSPATH=no lein repl
   :plugins [[venantius/ultra "0.6.0"]])
---
+++ src/aoc/year2017/day2.clj
@@ -0,0 +1,32 @@
+(ns aoc.year2017.day2
+ (:require
+   [clojure.string :as string]
+   [clojure.math.combinatorics :as combo]
+   [aoc.helpers :as helpers]))
+
+(defn part1 [input]
+  (->> (string/split input #"\n")
+       (map (fn [line]
+             (map #(Integer. %) (string/split line #"\t"))))
+       (map (fn [line]
+             (- (apply max line)
+                (apply min line))))
+       (apply +)))
+
+#_(part1 (helpers/get-input 2017 2))
+
+(defn part2 [input]
+  (->> (string/split input #"\n")
+       (map (fn [line]
+             (map #(Integer. %) (string/split line #"\t"))))
+       (map (fn [line]
+             (->> (combo/combinations line 2)
+                  (map sort)
+                  (map reverse)
+                  (filter (fn [[a b]]
+                           (= 0 (rem a b))))
+                  first
+                  (apply quot))))
+       (apply +)))
+
+#_(part2 (helpers/get-input 2017 2))
