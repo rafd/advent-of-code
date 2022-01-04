bfc01ba4f16d5229d864b50bd42ff54ecdc725f7
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-01-03 22:19:14 -0500

    Solve 2019 Day 1
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

---
+++ src/aoc/year2019/day1.clj
@@ -0,0 +1,31 @@
+(ns aoc.year2019.day1
+ (:require
+   [clojure.string :as string]
+   [aoc.helpers :as helpers]))
+
+(defn calculate-fuel [number]
+  (- (quot number 3) 2))
+
+(defn part1 [input]
+  (->> (string/split input #"\n")
+       (map #(Integer. %))
+       (map calculate-fuel)
+       (apply +)))
+
+#_(part1 (helpers/get-input 2019 1))
+
+(defn calculate-fuel-recursive [number]
+  (let [fuel (calculate-fuel number)]
+    (if (< 0 fuel)
+      (+ fuel (calculate-fuel-recursive fuel))
+      0)))
+
+#_(calculate-fuel-recursive 100756)
+
+(defn part2 [input]
+  (->> (string/split input #"\n")
+       (map #(Integer. %))
+       (map calculate-fuel-recursive)
+       (apply +)))
+
+#_(part2 (helpers/get-input 2019 1))
