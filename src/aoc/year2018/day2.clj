ff62cd0c3a1b147ef370cc76ac5b798ffe3b56d4
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-01-04 14:14:11 -0500

    Solve 2018 Day 2
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

---
+++ src/aoc/year2018/day2.clj
@@ -0,0 +1,38 @@
+(ns aoc.year2018.day2
+ (:require
+   [clojure.string :as string]
+   [clojure.set :as set]
+   [clojure.math.combinatorics :as combo]
+   [aoc.helpers :as helpers]))
+
+(defn part1 [input]
+  (let [freqs (->> (string/split input #"\n")
+                   (map frequencies)
+                   (map set/map-invert))]
+    (* (->> freqs
+            (filter (fn [f]
+                     (contains? f 2)))
+            count)
+       (->> freqs
+            (filter (fn [f]
+                     (contains? f 3)))
+            count))))
+
+#_(part1 (helpers/get-input 2018 2))
+
+(defn string-distance [a b]
+ (count (remove true? (map = a b))))
+
+#_(string-distance "aaa" "aaa")
+#_(string-distance "aab" "aaa")
+
+(defn part2 [input]
+  (->> (combo/combinations (string/split input #"\n") 2)
+       (filter (fn [[a b]]
+                (= 1 (string-distance a b))))
+       first
+       ((fn [[a b]]
+         (apply str (map first (filter (partial apply =) (map vector a b))))))))
+
+
+#_(part2 (helpers/get-input 2018 2))
