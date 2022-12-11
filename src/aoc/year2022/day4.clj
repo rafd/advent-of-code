76993f288486ff1df9f38c9dff660c84537f2f52
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-12-11 17:30:06 -0500

    Solve 2022 Day 3
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

---
+++ src/aoc/year2022/day4.clj
@@ -0,0 +1,57 @@
+(ns aoc.year2022.day4
+  (:require
+   [aoc.helpers :as h]
+   [hyperfiddle.rcf :refer [tests]]))
+
+(defn fully-contained? [a b c d]
+  (or (and (<= c a) (<= b d))
+      (and (<= a c) (<= d b))))
+
+(defn part1 [input]
+  (->> input
+       (map (fn [line]
+              (->> (re-matches #"(\d+)-(\d+),(\d+)-(\d+)" line)
+                   rest
+                   (map #(Integer. %))
+                   (apply fully-contained?))))
+       (filter true?)
+       count))
+
+(tests
+ (part1 (h/parse-input 2022 "4example" "\n")) := 2)
+
+#_(part1 (h/parse-input 2022 4 "\n"))
+
+(defn overlap? [start1 end1 start2 end2]
+  (or (<= start1 start2 end1)
+      (<= start1 end2 end1)
+      (<= start2 start1 end2)
+      (<= start2 end1 end2))) ;; last case redundant
+
+(defn part2 [input]
+  (->> input
+       (map (fn [line]
+              (->> (re-matches #"(\d+)-(\d+),(\d+)-(\d+)" line)
+                   rest
+                   (map #(Integer. %))
+                   (apply overlap?))))
+       (filter true?)
+       count))
+
+(tests
+ (part2 (h/parse-input 2022 "4example" "\n")) := 4)
+
+#_(part2 (h/parse-input 2022 4 "\n"))
+
+
+;;    |-------|
+;;      |-------|
+
+;;      |-------|
+;;    |-------|
+
+;;      |---|
+;;    |-------|
+
+;;    |-------|
+;;      |---|
