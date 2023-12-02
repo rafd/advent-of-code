64c82f2d89fbd1a2247b2ef15c3516e110a64c06
Rafal Dittwald <rafal.dittwald@gmail.com>
2023-12-02 00:52:36 -0500

    Split instruction fetching into two steps

--- src/aoc/fetch.clj
+++ src/aoc/fetch.clj
@@ -18,12 +18,10 @@
 
 #_(fetch-puzzle-input! 2023 1)
 
-(defn fetch-instructions! [year day]
-  (let [part1-target-file (io/file (str "resources/instructions/" year "day" day "part1.txt"))
-        part2-target-file (io/file (str "resources/instructions/" year "day" day "part2.txt"))]
-    (if (and (.exists part1-target-file)
-             (.exists part2-target-file))
-      (println "Instructions for " year day "already exist")
+(defn fetch-instructions! [year day part]
+  (let [target-file (io/file (str "resources/instructions/" year "day" day "part" part ".txt"))]
+    (if (.exists target-file)
+      (println "Instructions for " year day part "already exist")
       (e/with-chrome-headless driver
         (doto driver
           (e/go (str "https://adventofcode.com/" year "/day/" day))
@@ -32,7 +30,6 @@
                          :value (string/trim (slurp "AUTH_COOKIE"))})
           (e/go (str "https://adventofcode.com/" year "/day/" day))
           (e/wait-visible {:class "day-desc"}))
-        (spit part1-target-file (e/get-element-text driver {:class "day-desc" :index 1}))
-        (spit part2-target-file (e/get-element-text driver {:class "day-desc" :index 2}))))))
+        (spit target-file (e/get-element-text driver {:class "day-desc" :index part}))))))
 
-#_(fetch-instructions! 2023 1)
+#_(fetch-instructions! 2023 1 1)
