c65a0d935026f4272c7c519f5552907702b1a8d8
Rafal Dittwald <rafal.dittwald@gmail.com>
2023-12-03 00:34:29 -0500

    Solve 2023 Day 3
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

---
+++ src/aoc/year2023/day3.clj
@@ -0,0 +1,78 @@
+(ns aoc.year2023.day3
+  (:require
+   [hyperfiddle.rcf :as r]
+   [aoc.helpers :as h]))
+
+(defn part1 [input]
+  (let [parts-grid (->> input
+                        (mapv (fn [line]
+                                (->> (re-seq #"\d+|." line)
+                                     (mapcat (fn [num-string-or-dot]
+                                               (if-let [number (parse-long num-string-or-dot)]
+                                                 (let [uid [(gensym) number]]
+                                                   (repeat (count num-string-or-dot) uid))
+                                                 [nil])))
+                                     vec))))
+        grid (->> input (mapv vec))
+        symbol-coordinates (->> (for [x (range 0 (count input))
+                                      y (range 0 (count (first input)))]
+                                  (let [item (get-in grid [x y])]
+                                    (when (not (re-matches #"\d|\." (str item)))
+                                      [x y])))
+                                (remove nil?))]
+    (->> symbol-coordinates
+         (mapcat (fn [[x y]]
+                   (->> [[-1 -1] [-1 0] [-1 1]
+                         [0 -1]  [0 0]  [0 1]
+                         [1 -1]  [1 0] [1 1]]
+                        (map (fn [[dx dy]]
+                               (get-in parts-grid [(+ x dx) (+ y dy)]))))))
+         (remove nil?)
+         set
+         (map second)
+         (reduce +))))
+
+(r/tests
+ (part1 (h/parse-input 2023 "3example" "\n")) := 4361)
+
+#_(part1 (h/parse-input 2023 3 "\n"))
+
+(defn part2 [input]
+  (let [parts-grid (->> input
+                        (mapv (fn [line]
+                                (->> (re-seq #"\d+|." line)
+                                     (mapcat (fn [num-string-or-dot]
+                                               (if-let [number (parse-long num-string-or-dot)]
+                                                 (let [uid [(gensym) number]]
+                                                   (repeat (count num-string-or-dot) uid))
+                                                 [nil])))
+                                     vec))))
+        grid (->> input (mapv vec))
+        symbol-coordinates (->> (for [x (range 0 (count input))
+                                      y (range 0 (count (first input)))]
+                                  (let [item (get-in grid [x y])]
+                                    (when (not (re-matches #"\d|\." (str item)))
+                                      [x y])))
+                                (remove nil?))]
+    (->> symbol-coordinates
+         (filter (fn [[x y]]
+                   (= \* (get-in grid [x y]))))
+         (map (fn [[x y]]
+                (->> [[-1 -1] [-1 0] [-1 1]
+                      [0 -1]  [0 0]  [0 1]
+                      [1 -1]  [1 0] [1 1]]
+                     (map (fn [[dx dy]]
+                            (get-in parts-grid [(+ x dx) (+ y dy)])))
+                     (remove nil?)
+                     set)))
+         (filter (fn [found-parts]
+                   (= 2 (count found-parts))))
+         (map (fn [pair]
+                (* (second (first pair))
+                   (second (second pair)))))
+         (reduce +))))
+
+(r/tests
+ (part2 (h/parse-input 2023 "3example" "\n")) := 467835)
+
+#_(part2 (h/parse-input 2023 3 "\n"))
