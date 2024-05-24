08e919a42b8db93f4bc79d3daf1f84177e588a72
Rafal Dittwald <rafal.dittwald@gmail.com>
2024-05-24 14:17:13 -0400

    Solve 2016 Day 6

---
+++ src/aoc/year2016/day6.clj
@@ -0,0 +1,52 @@
+(ns aoc.year2016.day6
+  (:require
+    [clojure.string :as string]
+    [hyperfiddle.rcf :as rcf]
+    [aoc.helpers :as h]))
+
+(defn transpose [x]
+  (apply map vector x))
+
+(defn most-common [coll]
+  (key
+   (apply max-key
+          val
+          (frequencies coll))))
+
+(rcf/tests
+  (most-common [\a \b \a])
+  := \a
+  (most-common "aba")
+  := \a)
+
+(defn part-1 [input]
+  (->> input
+       string/split-lines
+       transpose
+       (map most-common)
+       (apply str)))
+
+(rcf/tests
+  (part-1 (h/get-input 2016 "6sample"))
+  := "easter")
+
+#_(part-1 (h/get-input 2016 6)) ;; usccerug
+
+(defn least-common [coll]
+  (key
+   (apply min-key
+          val
+          (frequencies coll))))
+
+(defn part-2 [input]
+  (->> input
+       string/split-lines
+       transpose
+       (map least-common)
+       (apply str)))
+
+(rcf/tests
+  (part-2 (h/get-input 2016 "6sample"))
+  := "advent")
+
+#_(part-2 (h/get-input 2016 6)) ;; cnvvtafc
