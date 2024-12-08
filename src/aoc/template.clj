21aff26a3f73ba8b61c2b68b23938213e76ad53b
Rafal Dittwald <rafal.dittwald@gmail.com>
2024-12-07 22:45:12 -0500

    Update template

--- src/aoc/template.clj
+++ src/aoc/template.clj
@@ -1,25 +1,27 @@
 (ns aoc.template
   (:require
+   [clojure.string :as string]
+   [clojure.set :as set]
    [hyperfiddle.rcf :as r]
    [aoc.helpers :as h]))
 
 (defn parse-line [line]
-  line)
+  (->> line))
 
 (defn part1 [input]
   (->> input
        (map parse-line)))
 
 (r/tests
- (part1 (h/parse-input 2023 "1example" "\n")) := nil)
+ (part1 (h/get-input 2023 "1example")) := nil)
 
-#_(part1 (h/parse-input 2023 1 "\n"))
+#_(part1 (h/get-input 2023 1))
 
 (defn part2 [input]
   (->> input
        (map parse-line)))
 
-(r/tests
- (part2 (h/parse-input 2023 "1example" "\n")) := nil)
+#_(r/tests
+   (part2 (h/get-input 2023 "1example")) := nil)
 
-#_(part2 (h/parse-input 2023 1 "\n"))
+#_(part2 (h/get-input 2023 1))
