906f748b92f4ff8451aa2d49d5c3864be2d0580d
Rafal Dittwald <rafal.dittwald@gmail.com>
2024-12-08 21:16:27 -0500

    Improve performance of 2024 Day 6 Part 1
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

--- src/aoc/year2024/day6.clj
+++ src/aoc/year2024/day6.clj
@@ -23,71 +23,84 @@
        (some (fn [position]
                (let [guard (get-in grid position)]
                  (when (->direction guard)
-                   {:guard guard
-                    :guard-position position}))))))
+                   position))))))
 
 (r/tests
- (find-guard [[\. \>] [\. \.]]) := {:guard \>
-                                    :guard-position [0 1]}
- (find-guard [[\. \<]]) := {:guard \<
-                            :guard-position [0 1]})
+ (find-guard [[\. \>] [\. \.]]) := [0 1]
+ (find-guard [[\. \<]]) := [0 1])
 
-(defn next-state [grid]
-  (let [{:keys [guard guard-position]} (find-guard grid)
-        next-guard-position (mapv + guard-position (->direction guard))]
-    (cond
-      ;; no guard
-      (nil? guard)
-      grid
+(defn next-state [{:state/keys [grid guard-position] :as state}]
+  (if (nil? guard-position)
+    state
 
-      ;; would hit obstacle, instead rotate
-      (= \# (get-in grid next-guard-position))
-      (assoc-in grid guard-position (rotate guard))
+    (let [guard (get-in grid guard-position)
+          next-guard-position (mapv + guard-position (->direction guard))]
+      (cond
+        ;; would hit obstacle, instead rotate
+        (= \# (get-in grid next-guard-position))
+        (assoc-in state (concat [:state/grid] guard-position) (rotate guard))
 
-      ;; move
-      (let [[next-y next-x] next-guard-position]
-        (and (<= 0 next-x (dec (count (first grid))))
-             (<= 0 next-y (dec (count grid)))))
-      (-> grid
-          (assoc-in guard-position \X)
-          (assoc-in next-guard-position guard))
+        ;; move
+        (let [[next-y next-x] next-guard-position]
+          (and (<= 0 next-x (dec (count (first grid))))
+               (<= 0 next-y (dec (count grid)))))
+        (-> state
+            (assoc-in (concat [:state/grid] guard-position) \X)
+            (assoc-in (concat [:state/grid] next-guard-position) guard)
+            (assoc :state/guard-position next-guard-position))
 
-      ;; going off map
-      :else
-      (-> grid
-          (assoc-in guard-position \X)))))
+        ;; going off map
+        :else
+        (-> state
+            (assoc-in (concat [:state/grid] guard-position) \X)
+            (assoc :state/guard-position nil))))))
+
+(defn grid->state [grid]
+  {:state/grid grid
+   :state/guard-position (find-guard grid)})
 
 (r/tests
- (next-state [[\X]]) := [[\X]]
- (next-state [[\^]]) := [[\X]]
- (next-state [[\>]]) := [[\X]]
- (next-state [[\v]]) := [[\X]]
- (next-state [[\<]]) := [[\X]]
- (next-state [[\> \.]]) := [[\X \>]]
- (next-state [[\. \<]]) := [[\< \X]]
- (next-state [[\.] [\^]]) := [[\^] [\X]]
- (next-state [[\v] [\.]]) := [[\X] [\v]]
+ (next-state (grid->state [[\X]]))
+ := (grid->state [[\X]])
+ (next-state (grid->state [[\^]]))
+ := (grid->state [[\X]])
+ (next-state (grid->state [[\>]]))
+ := (grid->state [[\X]])
+ (next-state (grid->state [[\v]]))
+ := (grid->state [[\X]])
+ (next-state (grid->state [[\<]]))
+ := (grid->state [[\X]])
+ (next-state (grid->state [[\> \.]]))
+ := (grid->state [[\X \>]])
+ (next-state (grid->state [[\. \<]]))
+ := (grid->state [[\< \X]])
+ (next-state (grid->state [[\.] [\^]]))
+ := (grid->state [[\^] [\X]])
+ (next-state (grid->state [[\v] [\.]]))
+ := (grid->state [[\X] [\v]])
 
- (next-state [[\> \#]]) := [[\v \#]])
+ (next-state (grid->state [[\> \#]])) := (grid->state [[\v \#]]))
 
-(defn simulate [grid]
-  (loop [grid grid]
-    (let [next-grid (next-state grid)]
-      (if (= next-grid grid)
-        grid
-        (recur next-grid)))))
+(defn simulate [state]
+  (loop [state state]
+    (let [state' (next-state state)]
+      (if (= state state')
+        state
+        (recur state')))))
 
 (defn part1 [input]
   (let [grid (->> input
                   string/split-lines
-                  (mapv vec))]
-    (->> (simulate grid)
+                  (mapv vec))
+        state (grid->state grid)]
+    (->> (simulate state)
+         :state/grid
          flatten
          (filter #{\X})
          count)))
 
 (r/tests
- (part1 (h/get-input 2024 "6example")) := 41)
+ (part1 (h/get-input 2024 "6example")) := 41
+ (part1 (h/get-input 2024 6)) := 5269)
 
-;; takes a while
-#_(part1 (h/get-input 2024 6)) := 5269
+
