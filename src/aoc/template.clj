6c00c52a0600e2ee4d10bf8ae20a49ae0245dc98
Rafal Dittwald <rafal.dittwald@gmail.com>
2024-12-12 23:03:47 -0500

    Update template

--- src/aoc/template.clj
+++ src/aoc/template.clj
@@ -5,23 +5,19 @@
    [hyperfiddle.rcf :as r]
    [aoc.helpers :as h]))
 
-(defn parse-line [line]
-  (->> line))
-
 (defn part1 [input]
   (->> input
-       (map parse-line)))
+       string/split-lines
+       (map (fn [line]
+              line))))
 
 (r/tests
- (part1 (h/get-input 2024 "3example")) := nil)
+ (part1 (h/get-input 2024 "3example")) := nil
+ #_(part1 (h/get-input 2024 3)) := nil)
 
-#_(part1 (h/get-input 2024 3))
-
-(defn part2 [input]
-  (->> input
-       (map parse-line)))
+#_(defn part2 [input])
 
 #_(r/tests
-   (part2 (h/get-input 2024 "3example")) := nil)
+   (part2 (h/get-input 2024 "3example")) := nil
+   #_(part2 (h/get-input 2024 3)) := nil)
 
-#_(part2 (h/get-input 2024 3))
