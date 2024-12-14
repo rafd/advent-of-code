421c61bf680c72e1a406f93ce0c3ff084a4c951b
Rafal Dittwald <rafal.dittwald@gmail.com>
2024-12-13 22:00:06 -0500

    Solve 2024 Day 11 Part 2
    Co-Authored-By: Canna Wen <cannawen@gmail.com>

--- src/aoc/year2024/day11.clj
+++ src/aoc/year2024/day11.clj
@@ -4,17 +4,24 @@
    [hyperfiddle.rcf :as r]
    [aoc.helpers :as h]))
 
+;; naive solution
+;; (list of stones => blink => new list of stones => ...)
+
+(defn halve-string [s]
+  (let [half-length (quot (count s) 2)]
+    [(subs s 0 half-length)
+     (subs s half-length)]))
+
 (defn blink [stones]
   (->> stones
        (mapcat (fn [stone]
-                 (if (= 0 stone)
+                 (cond
+                   (= 0 stone)
                    [1]
-                   (let [stone-string (str stone)
-                         half-length (quot (count stone-string) 2)]
-                     (if (even? (count stone-string))
-                       [(parse-long (subs stone-string 0 half-length))
-                        (parse-long (subs stone-string half-length))]
-                       [(* stone 2024)])))))))
+                   (even? (count (str stone)))
+                   (map parse-long (halve-string (str stone)))
+                   :else
+                   [(* stone 2024)])))))
 
 (defn solve [input n]
   (let [stones (->> (string/split input #" ")
@@ -24,10 +31,62 @@
                    stones
                    (range 0 n)))))
 
+#_(time (solve (h/get-input 2024 "11example") 25)) ;; 52ms
+#_(time (solve (h/get-input 2024 "11example") 26)) ;; 73ms
+#_(time (solve (h/get-input 2024 "11example") 27)) ;; 104ms
+#_(time (solve (h/get-input 2024 "11example") 28)) ;; 150ms
+
+#_(time (solve (h/get-input 2024 "11") 25)) ;; 150ms
+
+;; takes ??? for 75 blinks
+;; it is O(2^N)
+;; assuming each step is appx 1.4x the previous, 25=>75 steps is 52ms*1.4^50 = 17500 hours = 731 years (!!!)
+
+;; let's reframe the problem
+;; for a single stone, how stones will it create after N blinks?
+;; there will be a lot of re-calculations for the same stone + N blink count
+;; so... dynamic programming!
+
+(declare memo-stone-count)
+
+(defn stone-count
+  [blink-count stone]
+  (if (= blink-count 1)
+    (if (even? (count (str stone)))
+      2
+      1)
+    (cond
+      (= 0 stone)
+      (memo-stone-count (dec blink-count) 1)
+      (even? (count (str stone)))
+      (let [[a b] (halve-string (str stone))]
+        (+ (memo-stone-count (dec blink-count) (parse-long a))
+           (memo-stone-count (dec blink-count) (parse-long b))))
+      :else
+      (memo-stone-count (dec blink-count) (* stone 2024)))))
+
+(def memo-stone-count (memoize stone-count))
+
+(defn solve-fast [input n]
+  (let [stones (->> (string/split input #" ")
+                    (map parse-long))]
+    (apply + (map (partial stone-count n) stones))))
+
+#_(time (solve-fast (h/get-input 2024 "11example") 25)) ;; 8ms
+#_(time (solve-fast (h/get-input 2024 "11") 25)) ;; 8ms
+
+#_(time (solve-fast (h/get-input 2024 "11example") 75)) ;; 8ms
+#_(time (solve-fast (h/get-input 2024 "11") 75)) ;; 130ms
+
 (defn part1 [input]
-  (solve input 25))
+  (solve-fast input 25))
+
+(defn part2 [input]
+  (solve-fast input 75))
 
 (r/tests
- (part1 (h/get-input 2024 "11example")) := 55312)
+ (part1 (h/get-input 2024 "11example")) := 55312
+ (part1 (h/get-input 2024 11)) := 217812
+ (part2 (h/get-input 2024 "11example")) := 65601038650482
+ (part2 (h/get-input 2024 11)) := 259112729857522)
 
-#_(part1 (h/get-input 2024 11)) := 217812
